40e4159205fe4a8a2dd1a7345750d2c8cd70b4ee
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-01-10 21:25:47 +0900

    Edit-EditBotton

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,23 +1,24 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def new
+    @group = Group.new
+  end
 
   def index
   end
 
-  def new
-    @group = Group.new
-    @group.users << current_user
+  def edit
   end
 
   def create
     @group = Group.new(group_params)
+    @group.users << current_user
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
-  end
-
-  def edit
   end
 
   def update
@@ -28,13 +29,12 @@ class GroupsController < ApplicationController
     end
   end
 
-  private
-
-  def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
-  end
-
   def set_group
     @group = Group.find(params[:id])
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
