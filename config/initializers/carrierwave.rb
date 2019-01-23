e59165a64735ef183a452eff9b0d49b6f39eb522
kazuomiyamaguchi <kazuomiyamaguchi1110@gmail.com>
2019-01-23 21:20:34 +0900

    edit misscore

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,4 +14,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory  = 'upload-yamaguchi'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-yamaguchi'
+  config.cache_storage = :fog
 end
